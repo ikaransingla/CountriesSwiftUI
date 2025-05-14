250494e506dfbe0419c8dc331615a01e14a37bf3
ikaransingla <email.karan.23@gmail.com>
2025-05-14 13:55:29 +0530

    initial commit

---
+++ CountriesSwiftUI/CountriesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesSwiftUIApp.swift
+//  CountriesSwiftUI
+//
+//  Created by Karan Singla on 14/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CountriesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
